fe6ba271d7c3f0898c581e5b7dddbab3e87d5a93
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2023-01-11 18:04:02 +0200

    work on the banner section and footer

---
+++ .history/src/components/HomeOne/HomeOneHeader_20230111174921.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import logo7 from '../../assets/images/logo-7.png';
+import logo from '../../assets/images/logo.png';
+import StickyMenu from '../../lib/StickyMenu';
+import Navigation from '../Navigation';
+
+function HomeOneHeader({
+    lang,
+    darkEnable = false,
+    action,
+    langEnabled = false,
+    changeMode,
+    changeModeLan,
+    dark,
+    className,
+}) {
+    useEffect(() => {
+        StickyMenu();
+    });
+    return (
+        <header className={`appie-header-area appie-sticky ${className || ''}`}>
+            <div className="container">
+                <div className="header-nav-box">
+                    <div className="row justify-content-between align-items-center">
+                        <div className="col-lg-2 col-md-4 col-sm-5 col-6 order-1 order-sm-1">
+                            <div className="appie-logo-box">
+                                <a href="/">
+                                    {/* <img src={logo} alt="" /> */}
+                                    Solar Panels
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-1 col-sm-1 order-3 order-sm-2">
+                            <div className="appie-header-main-menu">
+                                <Navigation />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default HomeOneHeader;
---
+++ .history/src/components/Navigation_20230111175150.js
@@ -0,0 +1,60 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation({ lang = false }) {
+    return (
+        <>
+
+            <ul>
+                <li>
+                    Home <i className="fal fa-angle-down" />
+                </li>
+                <li>
+                    <Link to="/service">Service</Link>
+                </li>
+                <li>
+                    <a href="#">
+                        Pages <i className="fal fa-angle-down" />
+                    </a>
+                    <ul className="sub-menu">
+                        <li>
+                            <Link to="/about-us">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/about-us-another">About 2</Link>
+                        </li>
+                        <li>
+                            <Link to="/error">Error</Link>
+                        </li>
+                        <li>
+                            <Link to="/shops">Shops</Link>
+                        </li>
+                        <li>
+                            <Link to="/shops/shop-details">Shop details</Link>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">
+                        News <i className="fal fa-angle-down" />
+                    </a>
+                    <ul className="sub-menu">
+                        <li>
+                            <Link to="/news">News Page</Link>
+                        </li>
+                        <li>
+                            <Link to="/news/single-news">Single News</Link>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+
+        </>
+    );
+}
+
+export default Navigation;
